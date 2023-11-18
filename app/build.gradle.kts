e29cc774a552cba09978888236d10eadbd146d4b
Zakir.Mohammad <zmohammad@schoology.com>
2023-11-18 16:17:50 +0530

    refactor(App): Removing unwanted dependencies TN:10000

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -85,40 +85,38 @@ dependencies {
     // Production
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation("androidx.constraintlayout:constraintlayout:2.2.0-alpha13")
-    implementation("com.google.android.material:material:1.10.0")
-    implementation("androidx.databinding:databinding-runtime:8.1.3")
+
+
     implementation("com.google.dagger:hilt-android:2.48")
-    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
+    kapt("com.google.dagger:hilt-android-compiler:2.48")
+
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
-    kapt("com.google.dagger:hilt-android-compiler:2.48")
+    implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.activity:activity-compose:1.8.1")
+    implementation("androidx.compose.runtime:runtime-livedata")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
+    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    implementation("io.coil-kt:coil-compose:2.2.2")
+    implementation("androidx.paging:paging-compose:3.3.0-alpha02")
+
+
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.5")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.5")
+    implementation("androidx.navigation:navigation-compose:2.7.5")
+
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.1")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.1")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
+
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.10.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.1")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.1")
+
     implementation("com.jakewharton.timber:timber:5.0.1")
-    implementation("com.github.bumptech.glide:glide:4.16.0")
-    implementation("androidx.paging:paging-runtime:3.3.0-alpha02")
-    val composeBom = platform("androidx.compose:compose-bom:2023.10.01")
-    implementation(composeBom)
-    implementation("androidx.compose.material3:material3")
-    implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.activity:activity-compose:1.8.0")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
-    implementation("androidx.compose.runtime:runtime-livedata")
-    implementation("androidx.navigation:navigation-compose:2.7.5")
-    implementation("androidx.constraintlayout:constraintlayout-compose:1.0.1")
-    implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
-    implementation("androidx.paging:paging-compose:3.3.0-alpha02")
-    implementation("io.coil-kt:coil-compose:2.2.2")
+    implementation("androidx.paging:paging-runtime-ktx:3.3.0-alpha02")
+
 
     // Unit Tests
     testImplementation("junit:junit:4.13.2")
@@ -130,11 +128,9 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     androidTestImplementation("androidx.compose.ui:ui-test-junit4-desktop:1.6.0-beta01")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     androidTestImplementation("androidx.navigation:navigation-testing:2.7.5")
-    androidTestImplementation(composeBom)
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
 
     debugImplementation("com.squareup.leakcanary:leakcanary-android:2.12")
     debugImplementation("androidx.compose.ui:ui-tooling")
